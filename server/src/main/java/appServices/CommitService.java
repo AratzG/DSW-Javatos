e8cab14013b9184f8550aad6a88c0309aea5e703
Juan Cendoya <juancendoya@opendeusto.es>
2021-05-24 18:24:16 +0200

    appService

---
+++ server/src/main/java/appServices/CommitService.java
@@ -0,0 +1,43 @@
+package appServices;
+
+import ld.Commit;
+import ld.Repositorio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommitService {
+
+    private List<Commit> commits = new ArrayList<>();
+
+    public CommitService() {
+        //rellenar lista con BD
+    }
+
+    public synchronized void crearCommit() {
+        Commit commit = new Commit();
+        //añadir variables
+        commits.add(commit);
+    }
+
+    public synchronized void borrarCommit(int id) {
+        for(int i=0;i<commits.size();i++) {
+            if(commits.get(i).getIdCommit() == id) {
+                commits.remove(commits.get(i));
+                break;
+            }
+        }
+    }
+
+    public synchronized Commit devolverCommit(int id) {
+        Commit commit = null;
+
+        for(int i=0;i<commits.size();i++) {
+            if(commits.get(i).getIdCommit() == id) {
+                commit = commits.get(i);
+                break;
+            }
+        }
+        return commit;
+    }
+}
